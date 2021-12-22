dbf34b2bccb6041e92e33ab8d95dbff7f1fd229c
Diana Selyuk <selukdiana@gmail.com>
2021-12-22 17:28:55 +0300

    create program file

---
+++ MainPart/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TestGeneratorLib;
+
+namespace MainPart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             var pathToFolder = "D:\\спп 4\\TestGenerator-master\\MainPart\\Files";
+            var pathToGenerated = @"D:\\спп 4\\TestGenerator-master\\GeneratedTests\\GeneratedFiles";
+            
+            if (!Directory.Exists(pathToFolder))
+            {
+                Console.WriteLine($"Couldn't find directory {pathToFolder}");
+                return;
+            }
+            if (!Directory.Exists(pathToGenerated))
+            {
+                Directory.CreateDirectory(pathToGenerated);
+            }
+
+            var allFiles = Directory.GetFiles(pathToFolder);
+
+            var files = from file in allFiles
+                    where file.Substring(file.Length - 3) == ".cs"
+                    select file;
+            ITestGenerator generato = new TestsGenerator();
+            Task task =  new Pipeline().Generate(files, pathToGenerated,  generato);
+            task.Wait();
+            Console.WriteLine("end.");
+        }
+    }
+}
